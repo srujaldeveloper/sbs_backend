654c2fae4a2f6abed41ae1ad98377afe5384acba
Srujal <srujaldeveloper90@gmail.com>
2022-06-15 21:15:13 +0530

    changes

--- index.js
+++ index.js
@@ -19,37 +19,37 @@ app.use(bodyParser.json());
 // parse requests of content-type - application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// app.use('/uploads',express.static('uploads'));
-const db = require('./models')
-mongoose.connect(
-    db.url,
-    {
-        useNewUrlParser: true,
-        useUnifiedTopology: true
-    },
-    () => {
-        console.log("Connected to MongoDB");
-    }
-);
+// // app.use('/uploads',express.static('uploads'));
+// const db = require('./models')
+// mongoose.connect(
+//     db.url,
+//     {
+//         useNewUrlParser: true,
+//         useUnifiedTopology: true
+//     },
+//     () => {
+//         console.log("Connected to MongoDB");
+//     }
+// );
 
-require("./routes/user")(app);
-require("./routes/auth")(app);
-require("./routes/company")(app);
-require("./routes/seller")(app);
-require("./routes/currentstate")(app);
-require("./routes/customer")(app);
-require("./routes/category")(app);
-require("./routes/status")(app);
-require("./routes/project")(app);
-require("./routes/costtype")(app);
-require("./routes/template")(app);
-require("./routes/pricelist")(app);
-require("./routes/item")(app);
-require("./routes/projectdescription")(app);
-require("./routes/projectLabel")(app);
-require("./routes/pricelistcategory")(app);
-require("./routes/textcategory")(app);
-require("./routes/image")(app);
+// require("./routes/user")(app);
+// require("./routes/auth")(app);
+// require("./routes/company")(app);
+// require("./routes/seller")(app);
+// require("./routes/currentstate")(app);
+// require("./routes/customer")(app);
+// require("./routes/category")(app);
+// require("./routes/status")(app);
+// require("./routes/project")(app);
+// require("./routes/costtype")(app);
+// require("./routes/template")(app);
+// require("./routes/pricelist")(app);
+// require("./routes/item")(app);
+// require("./routes/projectdescription")(app);
+// require("./routes/projectLabel")(app);
+// require("./routes/pricelistcategory")(app);
+// require("./routes/textcategory")(app);
+// require("./routes/image")(app);
 
 
 app.get("/", function(req,res){
