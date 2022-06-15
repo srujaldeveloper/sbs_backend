17610c8eb1b72cc95dc535613197085eeb1eca1b
Srujal <srujaldeveloper90@gmail.com>
2022-06-15 22:22:42 +0530

    Changes on index

--- index.js
+++ index.js
@@ -1,39 +1,8 @@
-// const express = require("express");
-// require("./config/connection");
-// require("dotenv").config();
-// const bodyParser = require("body-parser");
-// const PORT = process.env.PORT || 8000;
-// const app = express();
-
-// app.use(express.static(__dirname + "/public/images"));
-// const cors = require("cors");
-
-// app.use(cors());
-// // parse requests of content-type - application/json
-// app.use(bodyParser.json());
-
-// // parse requests of content-type - application/x-www-form-urlencoded
-// app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(express.json());
-// // app.use('/uploads',express.static('uploads'));
-// app.use(
-//   require("./routes/user-service-routes/user-auth-service-routes"),
-//   require("./routes/user-service-routes/user-rest-service-routes")
-// );
-
-// app.get("/", function (req, res) {
-//   res.send("Working");
-// });
-
-// app.listen(PORT, function () {
-//   console.log("Listening on port " + PORT + ".");
-// });
-
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
-
 const app = express();
+
 app.get("/", function (req, res) {
   res.send("Working");
 });
@@ -64,7 +33,7 @@ mongoose.connect(
 
 require("./routes/user")(app);
 require("./routes/auth")(app);
-// require("./routes/company")(app);
+require("./routes/company")(app);
 // require("./routes/seller")(app);
 // require("./routes/currentstate")(app);
 // require("./routes/customer")(app);
