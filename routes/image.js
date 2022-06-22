c5b62dba3e4edfd04b80d9952ab37612f80567a0
Srujal <srujaldeveloper90@gmail.com>
2022-06-22 11:02:30 +0530

    Added env

--- routes/image.js
+++ routes/image.js
@@ -1,27 +1,31 @@
 const auth = require("../middleware/auth.middleware");
 
-module.exports = app => {
+module.exports = (app) => {
   const imageController = require("../controller/image.controller");
-  const { uploadFile } = require('../middleware/multer')
+  const { uploadFile } = require("../middleware/multer");
 
   var router = require("express").Router();
 
   //create new company
-  router.post("/image", uploadFile.single('logo'),  imageController.createImage);
+  router.post("/image", uploadFile.single("logo"), imageController.createImage);
 
   //update company
-  router.put("/image/:id", uploadFile.single('logo'),  imageController.updateImage);
+  router.put(
+    "/image/:id",
+    uploadFile.single("logo"),
+    imageController.updateImage
+  );
 
   //delete company
-  router.delete("/image/:id",  imageController.deleteImage);
+  router.delete("/image/:id", imageController.deleteImage);
 
   //get all company
-  router.get("/getAllImage",  imageController.getAllImage);
+  router.get("/getAllImage", imageController.getAllImage);
 
   //get compnay by id
-  router.get("/getImageById/:id",  imageController.getImageById);
-  
-  router.delete("/image",  imageController.deleteAllImage);
+  router.get("/getImageById/:id", imageController.getImageById);
 
-  app.use('/api', router);
+  router.delete("/image", imageController.deleteAllImage);
+
+  app.use("/api", router);
 };
